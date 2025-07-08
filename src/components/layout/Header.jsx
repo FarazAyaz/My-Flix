0e965ad0861e89550dc63d4577fbeafef720abee
FarazAyaz <ayazf313@gmail.com>
2025-07-08 22:30:25 +0500

    header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-        <h1>Header</h1>
+    <>
+      <section>
+        <div className="flex items-center justify-between p-4 bg-gray-800 text-white">
+          <h1 className="text-2xl font-bold">My Website</h1>
+          <nav>
+            <ul className="flex space-x-4">
+              <li><a href="/" className="hover:underline">Home</a></li>
+              <li><a href="/about" className="hover:underline">About</a></li>
+              <li><a href="/contact" className="hover:underline">Contact</a></li>
+              <li><a href="/movie" className="hover:underline">Movies</a></li>
+            </ul>
+          </nav>
+        </div>
+      </section>
       
-    </div>
+    </>
   )
 }
 
