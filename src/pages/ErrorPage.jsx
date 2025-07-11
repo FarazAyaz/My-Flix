128e087a06f02b40cf8237c343670e662f326ccf
FarazAyaz <ayazf313@gmail.com>
2025-07-11 23:12:15 +0500

    useNavigation

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { NavLink, useRouteError } from 'react-router-dom'
+import { useNavigate, useRouteError } from 'react-router-dom'
 
 const ErrorPage = () => {
     const error = useRouteError();
-    console.log(error);
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1);}
     if (error.status === 404) {
         return (
     <div className='flex flex-col items-center justify-center h-screen bg-gray-100 gap-6'>
@@ -15,9 +17,13 @@ const ErrorPage = () => {
           </figure>
             <h1 className='text-4xl font-bold '>Page Not Found</h1>
             
-               <NavLink to="/" className='mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors'>
+               {/* <NavLink to="/" className='mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors'>
                     Go to Home
-                </NavLink>
+                </NavLink> */}
+
+              <button className='mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors' onClick={handleGoBack}> Go Back
+
+              </button>
     </div>
   )
     }
